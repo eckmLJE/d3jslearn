6c6fa5f933930f297d029934fb226d36a20c9da3
Lucas Eckman <lucas.eckman@gmail.com>
2019-02-01 16:43:08 -0500

    confusion

---
+++ data.js
@@ -0,0 +1,64 @@
+boothReservations = [
+  {
+    orgName: "IOU",
+    boothId: 1
+  },
+  {
+    orgName: "PSA",
+    boothId: 2
+  },
+  {
+    orgName: "BTW",
+    boothId: 5
+  }
+];
+
+venueBooths = [
+  {
+    id: 1,
+    x: 50,
+    y: 50,
+    w: 50,
+    h: 65,
+    reservation: {
+      orgName: "BTW",
+      orgId: 90
+    }
+  },
+  {
+    id: 2,
+    x: 150,
+    y: 50,
+    w: 50,
+    h: 65,
+    reservation: {
+      orgName: "IOU",
+      orgId: 7
+    }
+  },
+  {
+    id: 3,
+    x: 250,
+    y: 50,
+    w: 50,
+    h: 65
+  },
+  {
+    id: 4,
+    x: 350,
+    y: 50,
+    w: 50,
+    h: 65
+  },
+  {
+    id: 5,
+    x: 450,
+    y: 50,
+    w: 50,
+    h: 65,
+    reservation: {
+      orgName: "IMHO",
+      orgId: 25
+    }
+  }
+];
---
+++ index.js
@@ -0,0 +1,66 @@
+var venue = d3.select(".venue");
+
+var group = venue
+  .selectAll("g")
+  .data(venueBooths)
+  .enter()
+  .append("g");
+
+var booth = group
+  .selectAll("rect")
+  .data(venueBooths)
+  .enter()
+  .append("rect")
+  .attr("x", function(d) {
+    return d.x;
+  })
+  .attr("y", function(d) {
+    return d.y;
+  })
+  .attr("width", function(d) {
+    return d.w;
+  })
+  .attr("height", function(d) {
+    return d.h;
+  })
+  .attr("fill", "none")
+  .attr("stroke-width", "2px")
+  .attr("stroke", "black");
+
+// var width = 420,
+//   barHeight = 20;
+
+// var x = d3
+//   .scaleLinear()
+//   .domain([0, d3.max(data)])
+//   .range([0, width]);
+
+// var chart = d3
+//   .select(".chart")
+//   .attr("width", width)
+//   .attr("height", barHeight * data.length);
+
+// var bar = chart
+//   .selectAll("g")
+//   .data(data)
+//   .enter()
+//   .append("g")
+//   .attr("transform", function(d, i) {
+//     return "translate(0," + i * barHeight + ")";
+//   });
+
+// bar
+//   .append("rect")
+//   .attr("width", x)
+//   .attr("height", barHeight - 1);
+
+// bar
+//   .append("text")
+//   .attr("x", function(d) {
+//     return x(d) - 3;
+//   })
+//   .attr("y", barHeight / 2)
+//   .attr("dy", ".35em")
+//   .text(function(d) {
+//     return d;
+//   });
