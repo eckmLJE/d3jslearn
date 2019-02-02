be7fa230cd8ad7e2e304a178517a7585e4c1faeb
Lucas Eckman <lucas.eckman@gmail.com>
2019-02-02 15:58:02 -0500

    panning and zooming

--- index.js
+++ index.js
@@ -1,47 +1,58 @@
 var svg = d3.select("#venue-svg");
-var tables;
-var tableRects;
 
-$("#set-groups-button").click(setGroups);
-$("#add-tables-button").click(addTables);
-$("#add-reservations-button").click(addReservations);
+// $("#set-groups-button").click(setGroups);
+// $("#add-tables-button").click(addTables);
+// $("#add-reservations-button").click(addReservations);
 
-function setGroups() {
-  tables = svg
-    .selectAll("g")
-    .data(venueBooths)
-    .enter()
-    .append("g")
-    .attr("transform", function(d) {
-      return "translate(" + d.x + " " + d.y + ")";
-    });
+svg
+  .append("rect")
+  .attr("width", 600)
+  .attr("height", 300)
+  .style("fill", "none")
+  .style("pointer-events", "all")
+  .call(
+    d3
+      .zoom()
+      .scaleExtent([1 / 2, 4])
+      .on("zoom", zoomed)
+  );
+
+function zoomed() {
+  g.attr("transform", d3.event.transform);
 }
 
-function addTables() {
-  tableRects = tables
-    .append("rect")
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", "2px")
-    .attr("width", function(d) {
-      return d.w;
-    })
-    .attr("height", function(d) {
-      return d.h;
-    })
-    .attr("fill", "none");
-}
+var g = svg.append("g");
 
-function addReservations() {
-  tableRects.attr("fill", function(d) {
-    return $.isEmptyObject(d.reservation) ? "none" : "#FF5733";
+var tables = g
+  .selectAll("g")
+  .data(venueBooths)
+  .enter()
+  .append("g")
+  .attr("transform", function(d) {
+    return "translate(" + d.x + " " + d.y + ")";
   });
 
-  tables
-    .append("text")
-    .text(function(d) {
-      return d.reservation.orgName;
-    })
-    .attr("dy", 20)
-    .attr("dx", 5)
-    .attr("font-size", "12px");
-}
+var tableRects = tables
+  .append("rect")
+  .attr("stroke", "steelblue")
+  .attr("stroke-width", "2px")
+  .attr("width", function(d) {
+    return d.w;
+  })
+  .attr("height", function(d) {
+    return d.h;
+  })
+  .attr("fill", "none");
+
+tableRects.attr("fill", function(d) {
+  return $.isEmptyObject(d.reservation) ? "none" : "#FF5733";
+});
+
+tables
+  .append("text")
+  .text(function(d) {
+    return d.reservation.orgName;
+  })
+  .attr("dy", 20)
+  .attr("dx", 5)
+  .attr("font-size", "12px");
